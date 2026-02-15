94bcf4ffb14d693d9db3ce3163eda323fadf6ae6
Pistonight <pistonknight@outlook.com>
2026-02-14 16:12:30 -0800

    enable sddm service first

--- packages/registry/src/packages/cde-hyprland/sddm.rs
+++ packages/registry/src/packages/cde-hyprland/sddm.rs
@@ -13,18 +13,21 @@ pub struct SddmAutoLoginConfig {
     user: String,
 }
 pub fn configure(cfg: &SddmConfig) -> cu::Result<()> {
-    let mut ini = opfs::IniFile::open("/etc/sddm.conf.d/default.conf")?;
+    opfs::sudo("systemctl", "enabling sddm service")?
+        .args(["enable", "sddm.service"])
+        .stdoe(cu::lv::P)
+        .stdin_null()
+        .wait_nz()?;
+    let mut ini = cu::check!(
+        opfs::IniFile::open("/etc/sddm.conf.d/default.conf"),
+        "failed to open sddm configuration - try rebooting the system"
+    )?;
     let section = ini.section_mut("Autologin");
     section.set("Relogin", "true");
     section.set("Session", "wayland");
     section.set("User", &cfg.autologin.user);
     ini.write()?;
 
-    opfs::sudo("systemctl", "enabling sddm service")?
-        .args(["enable", "sddm.service"])
-        .stdoe(cu::lv::P)
-        .stdin_null()
-        .wait_nz()?;
 
     Ok(())
 }
