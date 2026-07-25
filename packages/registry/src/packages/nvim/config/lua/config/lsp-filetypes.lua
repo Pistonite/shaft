dac51234cfebfabb65ab06d8d91f93cc93a14655
Pistonight <pistonknight@outlook.com>
2026-07-24 23:12:40 -0700

    config(nvim): the rust analyzer workaround still needed

--- packages/registry/src/packages/nvim/config/lua/config/lsp-filetypes.lua
+++ packages/registry/src/packages/nvim/config/lua/config/lsp-filetypes.lua
@@ -10,28 +10,28 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
 })
 
--- -- Remove if no longer needed
--- -- https://github.com/neovim/neovim/issues/30985 workaround for LSP error from rust-analyzer
--- for _, method in ipairs({
---     'textDocument/diagnostic',
---     'textDocument/semanticTokens/full/delta',
---     'textDocument/inlayHint',
---     'workspace/diagnostic'
--- }) do
---     local default_diagnostic_handler = vim.lsp.handlers[method]
---     vim.lsp.handlers[method] = function(err, result, context, config)
---         if err ~= nil then
---             if err.code == -32802 then
---                 return
---             end
---             if err.code == -32603 then
---                 return
---             end
---         end
---
---         return default_diagnostic_handler(err, result, context, config)
---     end
--- end
+-- Remove if no longer needed
+-- https://github.com/neovim/neovim/issues/30985 workaround for LSP error from rust-analyzer
+for _, method in ipairs({
+    'textDocument/diagnostic',
+    'textDocument/semanticTokens/full/delta',
+    'textDocument/inlayHint',
+    'workspace/diagnostic'
+}) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil then
+            if err.code == -32802 then
+                return
+            end
+            if err.code == -32603 then
+                return
+            end
+        end
+
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
 
 -- file type -> server[] registration
 -- server should be a key in the SERVERS map
